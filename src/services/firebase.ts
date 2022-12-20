a5edfa063062b1c1a99433ad6f6f329dbde32c29
Joshua Pearson <jpearson@ximasoftware.com>
2022-12-19 17:15:23 -0700

    Misc adjustments

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,7 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
-import { firebaseConfig } from "../environment/firebaseConfig";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAtLMz_5nWjMpCggg0SaytEOBqxUn9taUc",
+  authDomain: "savemypast-48c66.firebaseapp.com",
+  projectId: "savemypast-48c66",
+  storageBucket: "savemypast-48c66.appspot.com",
+  messagingSenderId: "350471324512",
+  appId: "1:350471324512:web:fc1d052966e876eb3f5405",
+  measurementId: "G-YMP1GSRT2B",
+};
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
