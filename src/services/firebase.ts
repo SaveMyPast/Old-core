cee2eb03f4b504f52216a163c36caa9102c5959d
Joshua Pearson <jpearson@ximasoftware.com>
2022-12-23 12:28:16 -0600

    removed unused iport

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,4 +1,3 @@
-import { UserInformation } from "./interfaces";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
