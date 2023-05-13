edcc2e516fdb1558166285f12ddbe2bbd0b19755
Joshua Pearson <jpearson@ximasoftware.com>
2023-05-13 12:53:48 -0600

    migrated to vite

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+
+/** @type {import('vite').UserConfig} */
+export default defineConfig({
+  build: {
+    // Relative to the root
+    outDir: "./build",
+  },
+});
