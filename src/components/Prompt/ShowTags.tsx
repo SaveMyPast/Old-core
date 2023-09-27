4cfd6cbe9816c451b711ce7ab7bb23029b77465a
Joshua Pearson <jpearson@ximasoftware.com>
2023-09-27 10:13:56 -0600

    fixed tags not collapsing

--- src/components/Prompt/ShowTags.tsx
+++ src/components/Prompt/ShowTags.tsx
@@ -1,12 +1,99 @@
-import { Chip, Stack } from '@mui/material';
+import { Popover, Chip, Stack, Box } from '@mui/material';
 import * as React from 'react';
 
+const CollapsedTags = ({ tags }: { tags: string[] }) => {
+    const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
+
+    const handlePopoverOpen = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handlePopoverClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+
+    return (
+        <>
+            <Chip
+                variant="outlined"
+                label={`+${tags.length}`}
+                onMouseEnter={handlePopoverOpen}
+                onMouseLeave={handlePopoverClose}
+                size="small"
+            />
+            <>
+                <Popover
+                    id="mouse-over-popover"
+                    sx={{
+                        pointerEvents: 'none',
+                    }}
+                    open={open}
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'center',
+                    }}
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'center',
+                    }}
+                    onClose={handlePopoverClose}
+                    disableRestoreFocus
+                >
+                    <Box className="p-2">
+                        <Stack direction={'column'} spacing={1}>
+                            {tags.map((tag, key) => (
+                                <Chip
+                                    variant="outlined"
+                                    label={tag}
+                                    key={key}
+                                    size="small"
+                                />
+                            ))}
+                        </Stack>
+                    </Box>
+                </Popover>
+            </>
+        </>
+    );
+};
+
 export const ShowTags = ({ tags }: { tags: string[] }) => {
+    const maxLength = 4;
+    const hiddenTags = tags.slice(maxLength);
+
+    if (tags.length > maxLength) {
+        return (
+            <>
+                <Stack direction={'row'} spacing={1}>
+                    {tags.slice(0, maxLength).map((tag, key) => (
+                        <Chip
+                            variant="outlined"
+                            label={tag}
+                            key={key}
+                            size="small"
+                        />
+                    ))}
+                    {hiddenTags.length > 0 && (
+                        <CollapsedTags tags={hiddenTags} />
+                    )}
+                </Stack>
+            </>
+        );
+    }
+
     return (
         <>
             <Stack direction={'row'} spacing={1}>
                 {tags.map((tag, key) => (
-                    <Chip variant="outlined" label={tag} key={key} />
+                    <Chip
+                        variant="outlined"
+                        label={tag}
+                        key={key}
+                        size="small"
+                    />
                 ))}
             </Stack>
         </>
@@ -23,7 +110,12 @@ export const ShowTagsVertical = ({ tags }: { tags: string[] }) => {
                 spacing={1}
             >
                 {tags.map((tag, key) => (
-                    <Chip variant="outlined" label={tag} key={key} />
+                    <Chip
+                        variant="outlined"
+                        label={tag}
+                        key={key}
+                        size="small"
+                    />
                 ))}
             </Stack>
         </>
--- src/components/Timeline/Selected/Selected.tsx
+++ src/components/Timeline/Selected/Selected.tsx
@@ -2,61 +2,64 @@ import * as React from 'react';
 
 import { userResponseStore } from '../../../services/stores/userResponseStore';
 import { useStore } from '@state-adapt/react';
-import { Divider, Stack, Typography, Chip } from '@mui/material';
+import { Divider, Stack, Typography } from '@mui/material';
 import DateRangeIcon from '@mui/icons-material/DateRange';
 import LocationIcon from '@mui/icons-material/LocationOn';
 import { Bookmarks } from '@mui/icons-material';
+import ShowTags from '../../Prompt/ShowTags';
 
 export const Selected = () => {
-	const userResponses = useStore(userResponseStore);
+    const userResponses = useStore(userResponseStore);
 
-	if (!userResponses.isSelected) {
-		return (
-			<>
-				<Typography variant="h5">Select a prompt to view.</Typography>
-			</>
-		);
-	}
+    if (!userResponses.isSelected) {
+        return (
+            <>
+                <Typography variant="h5">Select a prompt to view.</Typography>
+            </>
+        );
+    }
 
-	return (
-		<>
-			<Stack spacing={2}>
-				<Typography variant="h5">
-					{userResponses.selectedResponse?.prompt}
-				</Typography>
+    return (
+        <>
+            <Stack spacing={2}>
+                <Typography variant="h5">
+                    {userResponses.selectedResponse?.prompt}
+                </Typography>
 
-				<Stack direction="row" alignItems={'center'} spacing={2}>
-					<Stack direction={'row'} spacing={1} alignItems={'center'}>
-						<DateRangeIcon />
-						<Typography variant="caption">
-							{userResponses.selectedResponse?.year}
-						</Typography>
-						<Typography variant="caption">
-              Age {userResponses.selectedResponse?.age}
-						</Typography>
-					</Stack>
-					<Divider orientation="vertical" flexItem />
-					<Stack direction={'row'} spacing={1} alignItems={'center'}>
-						<LocationIcon />
-						<Typography variant="caption">
-							{userResponses.selectedResponse?.location}
-						</Typography>
-					</Stack>
-					<Divider orientation="vertical" flexItem />
-					<Stack direction={'row'} spacing={1}>
-						<Bookmarks />
-						{userResponses.selectedResponse?.tags.map(tag => (
-							<Chip variant="outlined" key={tag} label={tag} size="small" />
-						))}
-					</Stack>
-				</Stack>
+                <Stack direction="row" alignItems={'center'} spacing={2}>
+                    <Stack direction={'row'} spacing={1} alignItems={'center'}>
+                        <DateRangeIcon />
+                        <Typography variant="caption">
+                            {userResponses.selectedResponse?.year}
+                        </Typography>
+                        <Typography variant="caption">
+                            Age {userResponses.selectedResponse?.age}
+                        </Typography>
+                    </Stack>
+                    <Divider orientation="vertical" flexItem />
+                    <Stack direction={'row'} spacing={1} alignItems={'center'}>
+                        <LocationIcon />
+                        <Typography variant="caption">
+                            {userResponses.selectedResponse?.location}
+                        </Typography>
+                    </Stack>
+                    <Divider orientation="vertical" flexItem />
+                    <Stack direction={'row'} spacing={1}>
+                        <Bookmarks />
+                        {userResponses.selectedResponse?.tags && (
+                            <ShowTags
+                                tags={userResponses.selectedResponse?.tags}
+                            />
+                        )}
+                    </Stack>
+                </Stack>
 
-				<Typography variant="body1">
-					{userResponses.selectedResponse?.userResponse}
-				</Typography>
-			</Stack>
-		</>
-	);
+                <Typography variant="body1">
+                    {userResponses.selectedResponse?.userResponse}
+                </Typography>
+            </Stack>
+        </>
+    );
 };
 
 export default Selected;
